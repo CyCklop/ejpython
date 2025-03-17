f9c7d00353520f0c3cd949ec1f202593b5ed307a
CyCklop <ignacioandresflores06@gmail.com>
2025-03-16 22:11:29 -0300

    12323

---
+++ promedio.py
@@ -0,0 +1,6 @@
+num1 = int(input("Selecciona la primera nota: "))
+num2 = int(input("Selecciona la segunda nota: "))
+num3 = int(input("Selecciona la tercera nota: "))
+
+resultado = (num1+num2+num3)/3
+print(f"El resulado es:{resultado}")
