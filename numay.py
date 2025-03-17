04e5431e7035e970cfb94587073c93cad13bfd46
CyCklop <ignacioandresflores06@gmail.com>
2025-03-16 22:01:59 -0300

    111

---
+++ numay.py
@@ -0,0 +1,5 @@
+num1 = int(input("Selecciona el primer número: "))
+num2 = int(input("Selecciona el segundo número: "))
+num3 = int(input("Selecciona el tercer número: "))
+
+print("El número mayor es:",max(num1,num2,num3))
