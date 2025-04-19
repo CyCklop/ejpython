ea08658392ab0583db485db0f0ad4dab1a2eb6ce
CyCklop <ignacioandresflores06@gmail.com>
2025-04-19 17:53:45 -0400

    4353451231

--- poke.py
+++ poke.py
@@ -1,7 +1,7 @@
 pokedata = (50,50) # vida pokemon 1 & 2
 atq = [7,10] # ataques normales
 items = (15,10) # pocion,eter
-crit = [2] # duplicador de atq
+crit = (2) # duplicador de atq
 print("¡Selecciona el nombre de tu pokémon!")
 pokname = str(input(""))
 print(f"Eliges a {pokname}")
