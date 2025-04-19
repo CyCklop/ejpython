2e425fe0614201bd49503d8633e4524678aba5d2
CyCklop <ignacioandresflores06@gmail.com>
2025-04-19 18:25:39 -0400

    132qa

--- poke.py
+++
@@ -1,8 +0,0 @@
-pokedata = (50,50) # vida pokemon 1 & 2
-atq = [7,10] # ataques normales
-items = (15,10) # pocion,eter
-crit = (2) # duplicador de atq
-print("¡Selecciona el nombre de tu pokémon!")
-pokname = str(input(""))
-print(f"Eliges a {pokname}")
-print("¡El entrenador enemigo libera a Pikachu!")
