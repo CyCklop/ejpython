0754c9661a693373a4acce2b1bc4a6801484675b
CyCklop <ignacioandresflores06@gmail.com>
2025-04-21 10:39:40 -0400

    ewtdst

--- ejercicios_duoc/cant_alu_nota.py
+++ ejercicios_duoc/cant_alu_nota.py
@@ -1,12 +1,13 @@
 alum = int(input("Ingresa la cantidad total de alumnos: "))
 suma = 0
+
 for j in range(alum):
     cnotas = int(input(f"Ingresa la cantidad de notas del alumno {j+1}: "))
     for i in range(cnotas):
         nota = float(input(f"Ingresa la nota número {i+1} del alumno {j+1}: "))
         suma=suma+nota 
 prom=suma/cnotas
-print(f"El promedio total de los {j} alumnos son: {prom}")
+print(f"El promedio del alumno {j} es: {prom}")
 
 if prom>=4:
     print("Usted aprobo")
