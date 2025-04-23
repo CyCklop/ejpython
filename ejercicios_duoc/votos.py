9d75e902a9da16fb1d9ffaf61b1cad4cc8c54d4f
CyCklop <ignacioandresflores06@gmail.com>
2025-04-23 15:36:14 -0400

    dehecho

--- ejercicios_duoc/votos.py
+++ ejercicios_duoc/votos.py
@@ -1,21 +1,20 @@
 cant = int(input("Ingresa la cantidad de votantes: "))
-candi1 = 0
-candi2 = 0
+candid = [0,0]
 
 for i in range(cant):
     voto = int(input("Ingresa un voto (1- vardoc 2- xoda): "))
     if voto == 1:
-        candi1=candi1+1
+        candid[0]+=1
         print("¡Has votado por vardoc!")
     elif voto == 2:
-        candi2=candi2+1
+        candid[1]+=1
         print("¡Has votado por xoda!")
     else:
         print("Error seleccione un número valido")
 
-if candi1>candi2:
-    print(f"Las elecciones las ha ganado vardoc, con {candi1} votos")
-elif candi1==candi2:
+if candid[0]>candid[1]:
+    print(f"Las elecciones las ha ganado vardoc, con {candid[0]} votos")
+elif candid[0]==candid[1]:
     print(f"Las elecciones han sido empatadas hasta la segunda vuelta")
 else:
-    print(f"Las elecciones las ha ganado xoda, con {candi2} votos")
+    print(f"Las elecciones las ha ganado xoda, con {candid[1]} votos")
