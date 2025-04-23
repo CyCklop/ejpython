1b0c8f38870ea6edf4a93b732ab3f62cd03e72a7
CyCklop <ignacioandresflores06@gmail.com>
2025-04-23 16:12:23 -0400

    bro

--- ejercicios_duoc/vocales.py
+++ ejercicios_duoc/vocales.py
@@ -1,9 +1,12 @@
 pa = input("Escribe un texto: ")
-cont = [0,0]
+cont = [0,0,0]
 for i in pa:
     if i.lower() in "aeiou":
         cont[1]+=1
+    else:
+        cont[2]+=1
     cont[0]+=1
     print(i)
 print(f"La cantidad de caracteres es: {cont[0]}")
 print(f"La cantidad de vocales son: {cont[1]}")
+print(f"La cantidad de consonantes son: {cont[2]}")
