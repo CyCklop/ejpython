d6a0e146e14e92c6a5552e8a1a47beea7492abff
CyCklop <ignacioandresflores06@gmail.com>
2025-04-24 22:11:33 -0400

    123qwe

---
+++ ejercicios_propios/lista_append.py
@@ -0,0 +1,11 @@
+lista = [
+    ["Vessel","Album Debut","Twenty one Pilots",2013],
+    ["Blurryface","Album",2015],
+    ["Heathens","Single",2017],
+    ["Trench","Album",2018],
+    ["Scaled & Icy","Album",2021],
+    ["Clancy","Album",2024],
+    ["The line","Single",2024]
+]
+
+print(lista[0])
