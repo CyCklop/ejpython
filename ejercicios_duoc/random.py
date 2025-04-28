e18946696d2cf943adac79edf658b9011ac21e72
CyCklop <ignacioandresflores06@gmail.com>
2025-04-28 09:05:17 -0400

    12312

---
+++ ejercicios_duoc/random.py
@@ -0,0 +1,5 @@
+import random
+num = int(input("Selecciona un número inferior: "))
+num2 = int(input("Selecciona un número superior: "))
+numran = random.randint(num,num2)
+print(numran)
---
+++ ejercicios_duoc/whiletru.py
@@ -0,0 +1,6 @@
+x = 0
+while True:
+    print(f"repeticion {x+1}")
+    x+=1
+    if x == 25:
+        break
