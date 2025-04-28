5d603175fa6808c90b1d04fa7211c074ed55f616
CyCklop <ignacioandresflores06@gmail.com>
2025-04-28 10:54:26 -0400

    123ws

--- ejercicios_duoc/street_fighter1.py
+++ ejercicios_duoc/street_fighter1.py
@@ -3,42 +3,30 @@ import time
 hp1 = 50
 hp2 = 50
 turno = random.randint(1,2)
-critr = random.randint(1,5)
-daño = random.randint(3,15)
-crit = 2
 print("¡Selecciona los nombres de los luchadores!")
 ch1 = str(input("Ingresa el nombre del primer luchador: "))
 ch2 = str(input("Ingresa el nombre del segundo luchador: "))
 
 while hp1>0 and hp2>0:
     if turno % 2==0:
+        daño = random.randint(3,15)
         time.sleep(1)
         print(f"¡es el turno de {ch1}!")
         time.sleep(1)
-        if critr == 3:
-            print(f"¡{ch1} golpea a {ch2} (DMG CRITICO: {daño*crit})")
-            print(f"Vida de {ch2}")
-            print("/"*hp2)
-            hp2-=daño*crit
-        else:
-            print(f"¡{ch1} golpea a {ch2} (DMG: {daño})")
-            print(f"Vida de {ch2}")
-            print("/"*hp2)
+        print(f"¡{ch1} golpea a {ch2} (DMG: {daño})")
+        print(f"Vida de {ch2}")
+        print("/"*hp2)
         hp2-=daño
     else:
+        daño = random.randint(3,15)
         time.sleep(1)
         print(f"¡es el turno de {ch2}!")
         time.sleep(1)
-        if critr == 3:
-            print(f"¡{ch1} golpea a {ch2} (DMG CRITICO: {daño*crit})")
-            print(f"Vida de {ch1}")
-            print("/"*hp1)
-            hp1-=daño*crit
-        else:
-            print(f"¡{ch1} golpea a {ch2} (DMG: {daño})")
-            print(f"Vida de {ch1}")
-            print("/"*hp1)
+        print(f"¡{ch2} golpea a {ch1} (DMG: {daño})")
+        print(f"Vida de {ch1}")
+        print("/"*hp1)
         hp1-=daño
+    turno+=1
 if hp1>hp2:
     print(f"Ha ganado {ch1}")
 else:
