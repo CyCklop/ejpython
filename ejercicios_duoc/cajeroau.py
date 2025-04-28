ccedbe3055b8738ac578fa490c3e38e380c87366
CyCklop <ignacioandresflores06@gmail.com>
2025-04-28 11:12:52 -0400

    auxilio

---
+++ ejercicios_duoc/cajeroau.py
@@ -0,0 +1,7 @@
+import random 
+bill = ["dinero",5000,10000,20000]
+caja = 30
+
+for i in range (3):
+    user = str(input(f"Ingresa el nombre del usuario: "))
+    clave = int(input("Crea una clave de números: "))
