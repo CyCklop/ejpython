60c2d9ad426d298c22f8cddc5d24806d868f6eca
CyCklop <ignacioandresflores06@gmail.com>
2025-04-29 18:05:04 -0400

    32423esw

--- ejercicios_duoc/cajeroau.py
+++ ejercicios_duoc/cajeroau.py
@@ -1,7 +1,13 @@
 import random 
+import time
 bill = ["dinero",5000,10000,20000]
 caja = 30
 
-for i in range (3):
-    user = str(input(f"Ingresa el nombre del usuario: "))
-    clave = int(input("Crea una clave de números: "))
+clave = int(input("Cajero Automatico\nCrea una clave númerica para tu usuario: "))
+time.sleep(1)
+print("¡Clave guardada!")
+intento = int(input("Ingresa la clave creada: "))
+while intento!=clave:
+    intento = int(input("¡Clave incorrecta!\nIntenta nuevamente: "))
+else:
+    print("¡Bienvenido al cajero automatico!")
