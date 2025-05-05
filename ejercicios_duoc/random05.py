b1caf237859c6de04a2c382efef8dcf9a0fe4a56
CyCklop <ignacioandresflores06@gmail.com>
2025-05-05 10:28:48 -0400

    copiones duoc UC

--- ejercicios_duoc/random05.py
+++ ejercicios_duoc/random05.py
@@ -3,7 +3,7 @@ digit = int(input("Escriba el primer digito\n"))
 digit2 = int(input("Escriba el segundo digito\n"))
 while digit2<=digit:
     print("El segundo numero no puede ser menor o igual al primero")
-    digit2 = int(input("Escriba el segundo digito\n"))
+    digit2 = int(input("Escriba denuevo el segundo digito\n"))
 random_digit = random.randint(digit,digit2)
 for i in range(random_digit):
     print("▄")
