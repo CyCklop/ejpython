d441374d6840f25808222801c9984bc2b929b0a2
CyCklop <ignacioandresflores06@gmail.com>
2025-05-14 16:07:48 -0400

    1.1

--- ejercicios_propios/funcion.py
+++ ejercicios_propios/funcion.py
@@ -1,7 +1,13 @@
 # funciones python
-def promedio(n1,n2,n3):
-    resultado = n1 + n2 + n3
-    resultado = resultado/3
-    return resultado # al poner return se devolvera el valor en la variable
-llamada = promedio(2,2,5)
-print(llamada)
+#def promedio(n1,n2,n3):
+#    resultado = n1 + n2 + n3
+#    resultado = resultado/3
+#    return resultado # al poner return se devolvera el valor en la variable
+#llamada = promedio(2,2,5)
+#print(llamada)
+
+#def imprimir():
+#    print("Primera impresion")
+#    return #devolvera solo la primera impresión como si fuera un muro
+#    print("Segunda impresion")
+#imprimir()
