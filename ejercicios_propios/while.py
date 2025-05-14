b618652c4c31637a5ce829aaf3e2ab37654de316
CyCklop <ignacioandresflores06@gmail.com>
2025-05-14 15:42:49 -0400

    ejercicio post funcion

--- ejercicios_propios/while.py
+++ ejercicios_propios/while.py
@@ -2,5 +2,4 @@ y = 0
 while y < 6: #condición
     print("Repetición",y)
     y= y+1 #contador
-else:
-    print("Fin repetición, se llego al número limite:",y)
+print("Fin repetición, se llego al número limite:",y)
