01f21350082a46014ab50dfb946926e22067d940
CyCklop <ignacioandresflores06@gmail.com>
2025-05-24 08:47:22 -0400

    stressed out

--- ejercicios_propios_funcion/funcion_tienda.py
+++ ejercicios_propios_funcion/funcion_tienda.py
@@ -1,7 +1,8 @@
-import time
+import time 
 
 print("¡Bienvenido a Pystore!")
 name = str(input("Escribe tu nombre: "))
+
 
 def product():
     time.sleep(1)
@@ -10,31 +11,34 @@ def product():
 def carrito():
     total = 0
     while True:
-        product()
-        time.sleep(1)
-        op = int(input())
-        match op:
-            case 1:
-                print("Has comprado una lavadora")
-                total+=35000
-                print(f"Su subtotal es de {total}")
-            case 2:
-                print("Has comprado una guitarra")
-                total+=380000
-                print(f"Su subtotal es de {total}")
-            case 3:
-                print("Has comprado un parlante")
-                total+=25000
-                print(f"Su subtotal es de {total}")
-            case 4:
-                print("Has comprado un amplificador")
-                total+=65000
-                print(f"Su subtotal es de {total}")
-            case 5:
-                time.sleep(1)
-                print(f"¡Gracias por comprar en Pystore {name}!, su total neto es de {total}")
-                print(f"¡Gracias por comprar en Pystore {name}!, su total más IVA es de {total*1.19}")
-                break
-            case _:
-                print("Selecciona una opcion valida")
+        try:
+            product()
+            time.sleep(1)
+            op = int(input())
+            match op:
+                case 1:
+                    print("Has comprado una lavadora")
+                    total+=35000
+                    print(f"Su subtotal es de {total}")
+                case 2:
+                    print("Has comprado una guitarra")
+                    total+=380000
+                    print(f"Su subtotal es de {total}")
+                case 3:
+                    print("Has comprado un parlante")
+                    total+=25000
+                    print(f"Su subtotal es de {total}")
+                case 4:
+                    print("Has comprado un amplificador")
+                    total+=65000
+                    print(f"Su subtotal es de {total}")
+                case 5:
+                    time.sleep(1)
+                    print(f"¡Gracias por comprar en Pystore {name}!, su total neto es de {total}")
+                    print(f"¡Gracias por comprar en Pystore {name}!, su total más IVA es de {total*1.19}")
+                    break
+                case _:
+                    print("Selecciona una opcion valida")
+        except Exception:
+            print("Escribe un número")
 carrito()
