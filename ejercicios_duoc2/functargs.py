626c31fc6916ce593961d8bb324fbed892f37949
CyCklop <ignacioandresflores06@gmail.com>
2025-06-17 21:12:24 -0400

    2132weww

--- ejercicios_duoc2/functargs.py
+++ ejercicios_duoc2/functargs.py
@@ -3,8 +3,8 @@ def promedio(lista):
 cant = int(input("Ingresa la cantidad de notas: "))
 notas = []
 for i in range(cant):
-    op = float(input(f"Ingrese la nota {i+1}: "))
-    notas.append(op)
+    nota = float(input(f"Ingrese la nota {i+1}: "))
+    notas.append(nota)
 resultado = promedio(notas)
 resultado_round = round(resultado)
 print(f"El promedio es de: {resultado_round}")
