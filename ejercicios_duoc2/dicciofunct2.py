ef7df9509954e75d35853c9d542050954b89abf9
CyCklop <ignacioandresflores06@gmail.com>
2025-06-18 16:43:58 -0400

    8787

--- ejercicios_duoc2/dicciofunct2.py
+++ ejercicios_duoc2/dicciofunct2.py
@@ -2,15 +2,15 @@ personas = {
     1: {"nombre": "Ignacio Flores",
         "numero": [12345678],
         "edad": 19,
-        "trabajo": False},
+        "trabajo": True},
     2: {"nombre": "default",
         "numero": [0],
         "edad": 19,
-        "trabajo": False},
+        "trabajo": True},
     3: {"nombre": "Ignacio Flores",
         "numero": [12345678],
         "edad": 19,
-        "trabajo": False}
+        "trabajo": True}
 },
 
 
@@ -18,8 +18,7 @@ def ingresar(diccio):
     nom = input("Ingresa el nombre de un usuario: ")
     num = int(input("Ingresa el número de telefono del usuario"))
     age = int(input("Ingresa la edad del usuario: "))
-    job = input("¿Usted trabaja?: ")
-    diccio.insert(0,{"nombre": nom, "numero": num, "edad": age, "trabajo": job})
+    diccio.insert(0,{"nombre": nom, "numero": num, "edad": age})
 
 def mostrar(diccio):
     for i in enumerate(diccio):
@@ -29,23 +28,25 @@ def borrar():
     pass
 
 
-def main():
+def main(diccio):
     while True:
         try:
-            print("1) Ingresar perfil\n2) Mostrar perfiles\n3) Borrar perfil\n4) Salir")
+            print("1) Ingresar perfil\n2) Mostrar perfiles\n3) Actualizar persona\n4) Borrar perfil\n5) Salir")
             op = int(input(""))
             match op:
                 case 1:
-                    ingresar(personas)
+                    ingresar(diccio)
                 case 2:
-                    mostrar(personas)
+                    mostrar(diccio)
                 case 3:
                     pass
                 case 4:
                     pass
-                case _:
+                case 5:
                     print("Saliendo...")
                     break
+                case _:
+                    print("Error, seleccione una opción valida")
         except Exception:
             print("Error, ingrese un valor valido")
 main(personas)
