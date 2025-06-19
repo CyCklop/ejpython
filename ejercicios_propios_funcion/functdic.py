02dbfe0bbebb8d832b4a0fe46a6eebbc14872fdb
CyCklop <ignacioandresflores06@gmail.com>
2025-06-18 23:48:16 -0400

    3322

--- ejercicios_propios_funcion/functdic.py
+++ ejercicios_propios_funcion/functdic.py
@@ -1,43 +1,58 @@
+# MEMORIZA TODOOOO ESTO O CAGASTE
 users = {
     1: {"nombre": "Ignacio Flores","edad": 19,"numero":  [12345678],"trabajo": False},
     2: {"nombre": "Ignacio Flores","edad": 19,"numero":  [12345678],"trabajo": False},
     3: {"nombre": "Ignacio Flores","edad": 19,"numero":  [12345678],"trabajo": False}
 }
 
-def ingresar(diccio):
+def ingresar():
+    largo_dic = list(users.keys())[-1]
+    print(largo_dic)
     nom = input("Ingresa el nombre: ")
     age = int(input("Ingresa la edad: "))
     num = int(input("Ingresa el número telefonico: "))
-    job = int(input("¿Usted trabaja?\n1) Si\n2) No\nSeleccione una opción: "))
-    if job == 1:
-        pass
-    else:
-        pass
-
+    job = int(input("¿Usted trabaja?\n1) No\n2) Si\nSeleccione una opción: "))
+    users[largo_dic+1]={"nombre": nom, "edad": age, "numero": [num], "trabajo": bool(job-1)}
 
 def update():
-    pass
+    for key,value in users.items():
+        print(f"{key}-{value["nombre"]}")
+    op = int(input("Ingresa el número de usuario a actualizar: "))
+    usuario = users[op]
+    usuario["nombre"] = input("Ingresa el nombre: ")
+    usuario["edad"] = int(input("Ingresa la edad: "))
+    usuario["numero"] = int(input("Ingresa el número telefonico: "))
+    usuario["trabajo"] = int(input("¿Usted trabaja?\n1) No\n2) Si\nSeleccione una opción: "))
+
+
+
 def borrar():
-    pass
+    for key,value in users.items():
+        print(f"{key}-{value["nombre"]}")
+    op = int(input("Ingresa el numero del usuario a borrar: "))
+    users.pop(op)
+    print(f"Se ha borrado el usuario {op}")
+
+
 def show():
-    pass
+    for key,value in users.items():
+        print(f"{key}-{value}")
 
 
 
 def main():
-    global users
     while True:
         try:
             op = int(input("1) Ingresar usuario\n2) Actualizar usuario\n3) Borrar usuario\n4) Mostrar usuarios\n5) Salir\n"))
             match op:
                 case 1:
-                    ingresar(users)
+                    ingresar()
                 case 2:
-                    pass
+                    update()
                 case 3:
-                    pass
+                    borrar()
                 case 4:
-                    pass
+                    show()
                 case 5:
                     print("Saliendo..")
                     break
