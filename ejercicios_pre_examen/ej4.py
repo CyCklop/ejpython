4b411911f7fa4e14f206ec0d7f76a990a5e05e8a
CyCklop <ignacioandresflores06@gmail.com>
2025-07-08 22:59:48 -0400

    21312

--- ejercicios_pre_examen/ej4.py
+++ ejercicios_pre_examen/ej4.py
@@ -5,12 +5,25 @@ def numer(numero):
         return False
     if " " in numero:
         return False
-    for c in numero:
-        if numero.isnumeric():
-            pass
-        else:
-            return False
+    if numero.isnumeric():
+        pass
+    else:
+        return False
     return True
+
+def agregarnum(usuario):
+    while True:
+        nom = input("Ingresa el nombre de usuario para agregar otro número: ")
+        if nom in usuario:
+            num = input("Ingresa otro número de telefono: ")
+            if numer(num):
+                usuario[nom]["numero"].append(num)
+                print(f"Se ha agregado otro número al usuario {nom}")
+                break
+            else:
+                print("***Error, ingrese un número válido y sin espacios en blanco***")
+        else:
+            print(f"El nombre de usuario {nom}, no se encuentra en el sistema...")
 
 def ingresar(usuario):
     while True:
@@ -30,7 +43,7 @@ def ingresar(usuario):
 def eliminar(usuario):
     user = input("Ingresa el nombre del usuario a eliminar: ")
     if user in usuario.keys():
-        print(f"Eliminando usuario {user} ...")
+        print(f"Eliminando usuario {user}...")
         usuario.pop(user)
     else:
         print(f"El usuario {user}, no esta en el sistema...")
@@ -38,25 +51,27 @@ def eliminar(usuario):
 def ver(usuario):
     print("La lista de usuarios en el sistema es de:")
     for key,value in usuario.items():
-        print(f"Usuario: {key} --- Edad: {value["edad"]} --- Numeros de contacto: {value["numero"]}")
+        print(f"Usuario: {key} --- Edad: {value['edad']} --- Numeros de contacto: {value['numero']}")
 
 def menu():
     while True:
         try:
-            print("*****Ejemplo 4*****\n1) Ingresar usuario\n2) Eliminar usuario\n3) Ver usuarios\n4) Salir")
+            print("*****Ejemplo 4*****\n1) Ingresar usuario\n2) Ingresar otro número\n3) Eliminar usuario\n4) Ver usuarios\n5) Salir")
             op = int(input("Ingrese una opción: "))
             match op:
                 case 1:
                     ingresar(users)
                 case 2:
-                    eliminar(users)
+                    agregarnum(users)
                 case 3:
-                    ver(users)
+                    eliminar(users)
                 case 4:
+                    ver(users)
+                case 5:
                     print("Saliendo del sistema...")
                     break
                 case _:
                     print("Error, ingrese una opción válida")
         except Exception:
-            print("Error, ingrese un tipo de dato correcto")
+            print("***Error, ingrese un tipo de dato correcto***")
 menu()
