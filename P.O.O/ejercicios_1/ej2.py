4c314dc70bbd9a9b535b3e7b558e8bf2a02ae24d
CyCklop <ignacioandresflores06@gmail.com>
2025-07-22 02:46:52 -0400

    0.0.2

---
+++ P.O.O/ejercicios_1/ej2.py
@@ -0,0 +1,7 @@
+class personaje:
+    def __init__(self,nombre,tipo,arma,defensa):
+        self.nombre = nombre
+        self.tipo = tipo
+        self.arma = arma
+        self.defensa = defensa
+    pass
